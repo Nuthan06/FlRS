5f2ef93b0f2d42852217789c23ade1dbaffd07dc
Nuthan <nuthancreddy@gmail.com>
2021-01-20 21:02:53 +0530

    Updated as per heckpoint

--- IdeaProjects/FRS/src/FlightReservationSystem.java
+++ IdeaProjects/FRS/src/FlightReservationSystem.java
@@ -1,6 +1,7 @@
 import  java.util.*;
-public class FlightReservationSystem {
+public class FlightReservationSystem{
     public static void main(String[] args) {
+   /*
         Address a = new Address("Prakruthi nagar","Chitradurga","Karnataka");
         System.out.println(a.getAddressDetails());
         a.updateAddressDetails("Ambedkar Nagar@Chitradurga@Karnataka");
@@ -14,22 +15,34 @@ public class FlightReservationSystem {
         System.out.println(c.getContactDetails());
         c.setContactDetails("Bhavana","9899878799","sakeenassb@gmail.com");
         System.out.println(c.getContactDetails());
-
+    */
         Flight f = new Flight(87,"Air India",300,3);
         System.out.println(f.getFlightDetails());
         System.out.println(f.checkAvailability());
         f.updateBookedSeats(300);
         System.out.println(f.checkAvailability());
 
-        Passenger p = new Passenger(1,"red","9877665766","asd@gmail.com","gandhi street", "vizag","AP");
-        System.out.println(p.getAddress().getAddressDetails());
-        System.out.println(p.getContact().getContactDetails());
-        System.out.println(p.getId());
+        Passenger p = new Passenger("red","9877665766", "asd@gmail.com","gandhi street", "vizag","AP");
+        System.out.println("Address :"+p.getAddress());
+        System.out.println("Contact :"+p.getContact());
+        System.out.println("Id :"+p.getId());
+        System.out.println(Passenger.getIdCounter());
+        System.out.println(Passenger.getPassengerCount());
+        p.setAddress("ads","adads","ad" );
+        p.setContact("aps","314324342","s@gmail.com");
+
+        Passenger p1 = new Passenger("blue","9877665766",
+                "asd@gmail.com","gandhi street", "vizag","AP");
+        System.out.println(p1.getAddress());
+        System.out.println(p1.getContact());
+        System.out.println(p1.getId());
+        System.out.println(Passenger.getIdCounter());
+        System.out.println(Passenger.getPassengerCount());
 
         String arrivalTime = "21/01/01 23:23:34";
         String departureTime ="20/12/31 5:23:32";
         RegularTicket rt = new RegularTicket("13232","bhubaneshwar",
-                "Chitradurga",f,
+                "visakhapatnam",f,
                 departureTime,arrivalTime,p,213,6000,false,null);
         System.out.println(rt.checkStatusCancelled());
         System.out.println(rt.getFlightDuration());
@@ -40,13 +53,14 @@ public class FlightReservationSystem {
 
         String[] touristLocations = {"shimla","manali","ooty"};
         TouristTicket tt = new TouristTicket("13232","bhubaneshwar",
-                "Chitradurga",f,
-                departureTime,arrivalTime,p,213,6000,false,a,touristLocations);
+                "visakhapatnam",f,
+                departureTime,arrivalTime,p,213,6000,false,
+                " ",touristLocations);
         System.out.println(tt.checkStatusCancelled());
         System.out.println(tt.getFlightDuration());
         tt.setStatusCancelled();
         System.out.println(tt.checkStatusCancelled());
-        System.out.println(tt.getHotelAddress().getAddressDetails());
+        System.out.println(tt.getHotelAddress());
         for (String s: tt.touristLocations){
             System.out.println(s);
         }
